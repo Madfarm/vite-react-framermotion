daaaea50336e6358f7135ccc9037e119f22a08a2
AnthonyScott <anthony.scott1005@gmail.com>
2023-04-30 10:42:25 -0400

    Make text presentable

--- vite-framermotion-test/src/App.tsx
+++ vite-framermotion-test/src/App.tsx
@@ -3,14 +3,21 @@ import { motion } from 'framer-motion';
 import { Bruh } from './components/Bruh';
 
 function App() {
-  
+
 
   return (
     <>
-      <Bruh/>
-      <motion.div 
-      whileHover={{ scale: 1.2 }}
-      animate={{ x: 100 }}>Hellooooo</motion.div>
+      <Bruh />
+      <motion.div
+        whileHover={{ scale: 1.2 }}
+        animate={{ x: [200, 0] }}
+        drag
+        dragConstraints={{
+          top: -50000,
+          left: -50000,
+          right: 50000,
+          bottom: 50000,
+        }}>Hellooooo</motion.div>
     </>
   )
 }
--- vite-framermotion-test/src/components/Bruh.tsx
+++ vite-framermotion-test/src/components/Bruh.tsx
@@ -8,9 +8,23 @@ export const Bruh = () => (
       rotate: [0, 0, 270, 270, 0],
       borderRadius: ["20%", "20%", "50%", "50%", "20%"],
     }}
-    animate={{x: [-100, -200, -100, 0, 100]}}
+    animate={{x: [-200, 0] }}
+    drag
+    dragConstraints={{
+      top: -50000,
+      left: -50000,
+      right: 50000,
+      bottom: 50000,
+    }}
   >
-    <h3>My parents dont love me</h3>
+    <h3>Coding is sick</h3>
+    <motion.div className="Butt"
+    animate={{
+        opacity: [0, 0.1, 0.2, 0.3, 0.4, 0.5, .6, .7, .8, 1],
+    }}
+    
+    >
+        Brought to you by Vite</motion.div>
 
   </motion.div>
 )
